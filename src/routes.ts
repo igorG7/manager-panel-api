b569087e24fe32a92a39201025769168e0b05a77
igorG7 <igor.giovane.costa@gmail.com>
2026-06-08 11:58:02 -0300

    feat(routes): adiciona rotas referentes ao modulo “sale” no arquivo principal de rotas Ref: #19

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import userRoutes from "./modules/user/user-routes.ts";
 import productRoutes from "./modules/products/product-routes.ts";
 import stockMovementRoutes from "./modules/stock/stock-routes.ts";
 import clientRoutes from "./modules/client/client-routes.ts";
+import saleRoutes from "./modules/sale/sale-routes.ts";
 
 const routes = Router();
 
@@ -13,6 +14,7 @@ routes.use("/user", userRoutes);
 routes.use("/product", productRoutes);
 routes.use("/stock", stockMovementRoutes);
 routes.use("/client", clientRoutes);
+routes.use("/sale", saleRoutes);
 
 routes.use(launchError);
 
