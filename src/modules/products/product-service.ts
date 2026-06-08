8f5511e4849d7a39dde5d198ec9ed5a85b3be4d1
igorG7 <igor.giovane.costa@gmail.com>
2026-06-08 13:19:53 -0300

    feat(product): adiciona novos métodos ao service do módulo
    Métodos adicionados para suporte às operações de venda:
    
    - buildOperationBulk
    - updateMany
    - listIds
    
    Refs: #19

--- src/modules/products/product-service.ts
+++ src/modules/products/product-service.ts
@@ -1,8 +1,22 @@
+import type { ClientSession } from "mongoose";
 import { Conflict, NotFound } from "../../shared/utils/appErrors.ts";
 import type { IProduct } from "./domain/product-interface.ts";
 import Product from "./infrastructure/products.ts";
 
+type TypeProductMovement = {
+  product_id: string;
+  quantity: number;
+};
 class ProductService {
+  private buildOperationBulk = (dataArray: TypeProductMovement[], operation: "in" | "out") => {
+    return dataArray.map((item) => ({
+      updateOne: {
+        filter: { _id: item.product_id },
+        update: { $inc: { stock_quantity: operation === "in" ? +item.quantity : -item.quantity } },
+      },
+    }));
+  };
+
   private async productExist(product: any) {
     return await Product.findOne({
       _id: { $ne: product._id }, // ignora o próprio produto
@@ -23,6 +37,18 @@ class ProductService {
 
     return product;
   }
+
+  updateMany = async (data: any, operation: "in" | "out", session?: ClientSession) => {
+    const operationBulk = this.buildOperationBulk(data, operation);
+
+    await Product.bulkWrite(operationBulk, { ...(session && { session }) });
+  };
+
+  listIds = async (ids: string[], session?: ClientSession) => {
+    const products = await Product.find({ _id: { $in: ids } }).session(session as ClientSession);
+
+    return products;
+  };
 
   async update(data: Partial<IProduct>, id: string, userId: string) {
     // ! Propriedade packaging. A atualização dessa propriedade substitui o objeto inteiro, um merge com dot notation deve ser realizado para evitar.
