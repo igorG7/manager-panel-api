75dc5ebd016ad51fca2a4847bcc86eb88a143f4b
igorG7 <igor.giovane.costa@gmail.com>
2026-06-17 15:03:21 -0300

    feat(routes): implementa rotas do módulo dashboard
    Refs: #20

--- src/routes.ts
+++ src/routes.ts
@@ -7,6 +7,7 @@ import productRoutes from "./modules/products/product-routes.ts";
 import stockMovementRoutes from "./modules/stock/stock-routes.ts";
 import clientRoutes from "./modules/client/client-routes.ts";
 import saleRoutes from "./modules/sale/sale-routes.ts";
+import dashboardRoutes from "./modules/dashboard/dashboard-routes.ts";
 
 const routes = Router();
 
@@ -15,6 +16,7 @@ routes.use("/product", productRoutes);
 routes.use("/stock", stockMovementRoutes);
 routes.use("/client", clientRoutes);
 routes.use("/sale", saleRoutes);
+routes.use("/dashboard", dashboardRoutes);
 
 routes.use(launchError);
 
